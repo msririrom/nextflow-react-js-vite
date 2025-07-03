d420e8e0a6e4c3a397b2053cb03d903f1d0f02d7
msririrom <msririrom@gmail.com>
2025-07-03 02:40:32 +0000

    feat: add Hello and Goodbye components with username handling and greeting functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,63 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+let Hello = (props) => {
+  // if move <div> to new line , it will error
+  // because it is not a valid JSX syntax
+  // we can use () to wrap the return value
+  // or we can use <React.Fragment> to wrap the return value
+  // or we can use <></> to wrap the return value
+  // or we can use <div> to wrap the return value
+  // or we can use <section> to wrap the return value
+  // or we can use <article> to wrap the return value
+  // or we can use <main> to wrap the return value
+
+  if(props.username === 'undefined') {
+    return <div>
+      <h1 className='soft'>Hello : {props.username}</h1>
+      <p>Username is undefined</p>
+    </div>;
+  }else{
+ return <div>
+    <h1 className='soft'>Hello : {props.username}</h1>
+    </div>;
+  }
+
+ 
+}
+
+
+let Hello2 = (a,b) => {
+    return <div>
+      <p>username : {a.username}</p>
+      <p>password : {b.password}</p>
+    </div>;
+}
+
+function Goodbye() {
+  return (
+  <div>
+    <h1>Goodbye</h1>
+  </div>
+  )
+}
+
 function App() {
   
   let city = 'Bangkok';
+
+  const onSawasdee = () => {
+    console.log('Sawasdee Ka');
+  }
+
   return (
     <div>
       <h1>Helloooooooo Malinee</h1>
       <h1 id='header' className='danger' >Are you live in {city}</h1>
+      <Hello username="Maple"></Hello>
+      <Hello username={city}></Hello>
+      <Hello2 username="AAA" password="BBB"></Hello2>
+      <button onClick={onSawasdee}>SAWASDEE</button>
     </div>
   );
 }
